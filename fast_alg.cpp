ad54a1ee7f54d80713a25ab130eb4479a54d98d4
kameranis <kameranis@gmail.com>
2024-02-15 14:51:54 -0600

    Refactor algorithm into functions.

--- fast_alg.cpp
+++ fast_alg.cpp
@@ -10,23 +10,25 @@
 #include <chrono>
 #include <ctime>
 
+#include <Eigen/Dense>
+#include <Eigen/Sparse>
 
 using namespace std;
 
-void read_hypergraph(char filename[], int &n, int &m, vector<double> &degree, vector<vector<int> > &hypergraph) {
+void read_hypergraph(char filename[], int &n, int &m, std::vector<double> &degree, std::vector<vector<int> > &hypergraph) {
     int fmt;
-    string line;
-    ifstream input_file;
+    std::string line;
+    std::ifstream input_file;
     input_file.open(filename);
     input_file >> m >> n >> fmt;
     getline(input_file, line);
 
     // Read hyperedges
     for(int i = 0; i < m; i++) {
-        vector<int> hyperedge;
+        std::vector<int> hyperedge;
         int node;
         getline(input_file, line);
-        istringstream iss(line);
+        std::istringstream iss(line);
         while(iss >> node) {
             hyperedge.push_back(node - 1);
         }
@@ -42,15 +44,15 @@ void read_hypergraph(char filename[], int &n, int &m, vector<double> &degree, ve
     input_file.close();
 }
 
-int read_labels(char filename[], int n, vector<int> &labels) {
-    ifstream label_file;
+int read_labels(char filename[], int n, std::vector<int> &labels) {
+    std::ifstream label_file;
     label_file.open(filename);
-    string line;
+    std::string line;
     getline(label_file, line);
-    istringstream iss(line);
+    std::istringstream iss(line);
     int label;
     int label_count;
-    map<int, int> label_map;
+    std::map<int, int> label_map;
     for(label_count = 0; iss >> label; label_count++) {
         label_map[label] = label_count;
     }
@@ -70,7 +72,7 @@ inline double fmin(double a, double b) {
     return a < b ? a : b;
 }
 
-void diffusion(int n, int m, const vector<double> degree, const vector<vector<int>> hypergraph, double x[], double s[], int T, double lambda, double h, double solution[]) {
+void diffusion(int n, int m, const std::vector<double> degree, const std::vector<std::vector<int>> hypergraph, double x[], double s[], int T, double lambda, double h, double solution[]) {
     double gradient[n];
     for(int t = 0; t < T; t++) {
         for(int i = 0; i < n; i++)
@@ -111,7 +113,7 @@ void diffusion(int n, int m, const vector<double> degree, const vector<vector<in
     }
 }
 
-double compute_fx(int n, int m, vector<double> degree, vector<vector<int>> hypergraph, double x[], double s[], double lambda) {
+double compute_fx(int n, int m, std::vector<double> degree, std::vector<std::vector<int>> hypergraph, double x[], double s[], double lambda) {
     double fx = 0;
     for(int j = 0; j < m; j++) { 
         if(hypergraph[j].size() == 0)
@@ -130,14 +132,70 @@ double compute_fx(int n, int m, vector<double> degree, vector<vector<int>> hyper
     return fx;
 }
 
+void run_diffusions(int n, int m, std::vector<double> degree, std::vector<std::vector<int>> hypergraph, std::string graph_name, int label_count, std::vector<int> labels, int repeats, int T, double lambda, double h, int minimum_revealed, int step, int maximum_revealed) {
+    double seed[n];
+    double x[n];
+    double solution[n];
+    double max_sol[n];
+    double fx;
+    int predicted_labels[n];
 
+    int order[n];
+    for(int i = 0; i < n; i++) {
+        order[i] = i;
+    }
+
+    srand(unsigned(time(0)));
+    for(int i = 0; i < n; i++) {
+        x[i] = seed[i] = solution[i] = 0;
+        max_sol[i] = -INFINITY;
+    }
+
+    // Multiple repeats
+    for(int repeat = 0; repeat < repeats; repeat++) {
+        // Run for different number of revealed
+        random_shuffle(order, order+n);
+        for(int revealed = minimum_revealed; revealed <= maximum_revealed; revealed += step) {
+            const auto start{chrono::steady_clock::now()};
+            fx = 0;
+            for(int i = 0; i < n; i++) {
+                predicted_labels[i] = 0;
+                max_sol[i] = -INFINITY;
+            }
+            for(int r = 0; r < label_count; r++) {
+                for(int i = 0; i < n; i++) {
+                    x[i] = seed[i] = solution[i] = 0;
+                }
+                for(int i = 0; i < revealed; i++) {
+                    int node = order[i];
+                    seed[node] = lambda * (2 * (labels[node] == r) - 1);
+                }
+                diffusion(n, m, degree, hypergraph, x, seed, T, lambda, h, solution);
+                for(int i = 0; i < n; i++)
+                    if(max_sol[i] < solution[i]) {
+                        predicted_labels[i] = r;
+                        max_sol[i] = solution[i];
+                    }
+                fx += compute_fx(n, m, degree, hypergraph, solution, seed, lambda);
+            }
+            const auto end{chrono::steady_clock::now()};
+            const chrono::duration<double> time{end - start};
+            double error = 0;
+            for(int i = 0; i < n; i++)
+                error += (predicted_labels[i] != labels[i]);
+            // for(int t=0; t < T; t++)
+            //    cout << t+1 << " " << fx[t] << endl;
+            cout << graph_name << ",C++," << repeat << "," << revealed << "," << lambda << "," << time.count() << "," << error / n << "," << fx / label_count << endl;
+        }
+    }
+}
 
 int main(int argc, char* argv[]) {
     int n;
     int m;
-    vector<vector<int>> hypergraph;
-    vector<double> degree;
-    vector<int> labels;
+    std::vector<std::vector<int>> hypergraph;
+    std::vector<double> degree;
+    std::vector<int> labels;
     if(argc != 10) {
         cerr << "Wrong number of arguments" << endl;
         return -1;
@@ -151,58 +209,12 @@ int main(int argc, char* argv[]) {
     int minimum_revealed = stoi(argv[6]);
     int step = stoi(argv[7]);
     int maximum_revealed = stoi(argv[8]);
+    if(maximum_revealed > n) {
+        perror("Cannot reveal more nodes than total number of nodes.");
+        exit(1);
+    }
     int repeats = stoi(argv[9]);
 
-    double seed[label_count][n+2];
-    double x[label_count][n+2];
-    double solution[label_count][n+2];
-    double fx;
-    int predicted_labels[n+2];
-
-    int order[n];
-    for(int i = 0; i < n; i++) {
-        order[i] = i;
-    }
-
-    srand(unsigned(time(0)));
-    for(int r = 0; r < label_count; r++) {
-        for(int i = 0; i < n; i++) {
-            x[r][i] = seed[r][i] = solution[r][i] = 0;
-        }
-    }
-
-    // Multiple repeats
-    for(int repeat = 0; repeat < repeats; repeat++) {
-        // Run for different number of revealed
-        random_shuffle(order, order+n);
-        for(int revealed = minimum_revealed; revealed <= maximum_revealed; revealed += step) {
-            const auto start{chrono::steady_clock::now()};
-            fx = 0;
-            for(int i = 0; i < n; i++)
-                predicted_labels[i] = 0;
-            for(int r = 0; r < label_count; r++) {
-                for(int i = 0; i < n; i++) {
-                    x[r][i] = seed[r][i] = solution[r][i] = 0;
-                }
-                for(int i = 0; i < revealed; i++) {
-                    int node = order[i];
-                    seed[r][node] = lambda * (2 * (labels[node] == r) - 1);
-                }
-                diffusion(n, m, degree, hypergraph, x[r], seed[r], T, lambda, h, solution[r]);
-                for(int i = 0; i < n; i++)
-                    if(solution[predicted_labels[i]][i] < solution[r][i])
-                        predicted_labels[i] = r;
-                fx += compute_fx(n, m, degree, hypergraph, solution[r], seed[r], lambda);
-            }
-            const auto end{chrono::steady_clock::now()};
-            const chrono::duration<double> time{end - start};
-            double error = 0;
-            for(int i = 0; i < n; i++)
-                error += (predicted_labels[i] != labels[i]);
-            // for(int t=0; t < T; t++)
-            //    cout << t+1 << " " << fx[t] << endl;
-            cout << argv[1] << ",C++," << repeat << "," << revealed << "," << lambda << "," << time.count() << "," << error / n << "," << fx / label_count << endl;
-        }
-    }
+    run_diffusions(n, m, degree, hypergraph, argv[1], label_count, labels, repeats, T, lambda, h, minimum_revealed, step, maximum_revealed);
     return 0;
 }
